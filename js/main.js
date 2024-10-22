b036423febb60afff092fe6a8f828fd77e282d4e
MarcoProstamo <Marcopros1201@gmail.com>
2024-10-22 14:07:36 +0200

    Feat → Toggle Lamp

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const lampEl = document.querySelector("#lamp");
+const toggleLampEl = document.querySelector("#toggleLamp");
+
+toggleLampEl.addEventListener("click", () => {
+  lampEl.attributes.src.value === "./img/white_lamp.png"
+    ? (lampEl.attributes.src.value = "./img/yellow_lamp.png")
+    : (lampEl.attributes.src.value = "./img/white_lamp.png");
+});
