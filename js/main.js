d34e6a0a3b760e685bf3594fe6dcaa9455bccc3b
MarcoProstamo <Marcopros1201@gmail.com>
2024-10-22 14:57:02 +0200

    Refactor → Cleaner implementation

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 const lampEl = document.querySelector("#lamp");
 const toggleLampEl = document.querySelector("#toggleLamp");
 
-// Required for reasons
-toggleLampEl.innerHTML = "Accendi";
-
 toggleLampEl.addEventListener("click", () => {
   lampEl.attributes.src.value === "./img/white_lamp.png"
     ? (lampEl.attributes.src.value = "./img/yellow_lamp.png")
     : (lampEl.attributes.src.value = "./img/white_lamp.png");
-  toggleLampEl.innerHTML === "Accendi"
+  toggleLampEl.innerHTML.includes("Accendi")
     ? (toggleLampEl.innerHTML = "Spegni")
     : (toggleLampEl.innerHTML = "Accendi");
 });
