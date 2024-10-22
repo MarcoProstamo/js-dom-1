ca0e1336949c2c4c29b03c7474a22c73f459fc89
MarcoProstamo <Marcopros1201@gmail.com>
2024-10-22 14:37:38 +0200

    Feat → Switch Changes Name OnClick

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 const lampEl = document.querySelector("#lamp");
 const toggleLampEl = document.querySelector("#toggleLamp");
 
+// Required for reasons
+toggleLampEl.innerHTML = "Accendi";
+
 toggleLampEl.addEventListener("click", () => {
   lampEl.attributes.src.value === "./img/white_lamp.png"
     ? (lampEl.attributes.src.value = "./img/yellow_lamp.png")
     : (lampEl.attributes.src.value = "./img/white_lamp.png");
+  toggleLampEl.innerHTML === "Accendi"
+    ? (toggleLampEl.innerHTML = "Spegni")
+    : (toggleLampEl.innerHTML = "Accendi");
 });
